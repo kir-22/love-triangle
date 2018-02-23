ac097266a57e0864bd9eba2a32d5828879500ced
kir-22 <kirill89kireew@gmail.com>
2018-02-24 00:02:31 +0300

    Task2

--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+    var count = 0;
+    for (let i = 0; i < preferences.length; i++) {
+        let spok=i+1;
+        let a=preferences[i]-1;
+        let b=preferences[a]-1;
+        if (spok == preferences[b] && preferences[i] != preferences[a] ) {
+            count++;
+            delete preferences[i];
+            delete preferences[a];
+            delete preferences[b];
+        }
+    }
+    return count;
 };
+
